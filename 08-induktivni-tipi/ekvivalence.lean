6fb97f26b3a0af321eecb406ed9f427f470c2965
Luka Urbanč <urbancluka3@gmail.com>
2024-12-09 10:21:52 +0100

    add ekvivalence

--- 08-induktivni-tipi/ekvivalence.lean
+++ 08-induktivni-tipi/ekvivalence.lean
@@ -1,3 +1,20 @@
+-- indukcija po levem: 
+-- d (xs @ ys) = d xs + d ys 
+-- _________________________
+
+
+-- 1.) xs = [] 
+-- d ([] @ ys) = d (ys) = d ys ([] @ ys = ys iz operacije, ki je ze definirana) (d[] = 0, kar ze imamo kot operacija) 
+
+-- 2.) xs
+-- d(xs @ ys) = d xs + d ys (predpostavka)
+-- d ((x :: xs) @ ys) = d(x :: xs) + d ys 
+-- po (2) -> d(x :: (xs @ ys)) = d(x::xs) + d ys
+-- po (6) -> 1 + d(xs @ ys) = (1 + d xs) + d ys
+-- IP -> 1 + (d xs + d ys) = (1 + d xs) + d ys 
+-- asociativnost sestevanja -> dokazano 
+-- TO JE PRIMER ROCNEGA DELA ZA ENO TRDITEV 
+
 def concat {A : Type} : List A → List A → List A :=
   fun xs ys =>
     match xs with
@@ -7,22 +24,39 @@ def concat {A : Type} : List A → List A → List A :=
 #check (concat ["a", "b"] ["c", "d"])
 
 def reverse {A : Type} : List A → List A :=
-  sorry
+  fun xs => 
+    match xs with 
+    | [] => []
+    | x :: xs' => concat (reverse xs') [x]
 
 
 #check (reverse ["a", "b", "c", "d"])
 
 def length {A : Type} : List A → Nat :=
-  sorry
+  fun xs =>
+    match xs with 
+    | [] => 0
+    | x :: xs' => 1 + length xs'
 
 
 #check (length ["a", "b", "c", "d"])
 
 theorem trd1  {A : Type} {x : A} : reverse [x] = [x] :=
-  sorry
+  by
+    rw [reverse]
+    rfl
 
 theorem trd2 {A : Type} {xs ys : List A} : length (concat xs ys) = length xs + length ys :=
-  sorry
+  by
+    induction xs with 
+    | nil => 
+            simp [concat]
+            simp [length]
+    | cons x xs' ih => 
+            simp [concat]
+            simp [length]
+            rw [ih]
+            simp [Nat.add_assoc]
 
 -- Tega poznamo že iz predavanj
 theorem trd3 {A : Type} {xs : List A} : concat xs [] = xs :=
@@ -35,16 +69,53 @@ theorem trd3 {A : Type} {xs : List A} : concat xs [] = xs :=
       rw [ih]
 
 theorem trd4 {A : Type} {xs ys zs : List A} : concat (concat xs ys) zs = concat xs (concat ys zs) :=
-  sorry
+  by 
+    induction xs with 
+    | nil => 
+            simp [concat]
+    | cons x xs' ih => 
+            simp [concat]
+            rw [ih]
 
 theorem trd5 {A : Type} {xs ys : List A} : reverse (concat xs ys) = concat (reverse ys) (reverse xs) :=
-  sorry
+  by 
+    induction xs with 
+    | nil => 
+            simp [concat]
+            simp [reverse]
+            rw [trd3]
+    | cons x xs' ih => 
+            simp [concat]
+            simp [reverse]
+            rw [ih]
+            rw [trd4]
+
 
 theorem trd6 {A : Type} {xs : List A} : length (reverse xs) = length xs :=
-  sorry
+  by 
+    induction xs with 
+    | nil => 
+            simp [reverse]
+    | cons x xs' ih => 
+            simp [reverse]
+            rw [trd2]
+            rw [ih]
+            simp [length]
+            simp [Nat.add_comm]
+            
 
 theorem trd7 {A : Type} {xs : List A} : reverse (reverse xs) = xs :=
-  sorry
+  by
+    induction xs with 
+    | nil => 
+            simp [reverse]
+    | cons x xs' ih =>
+            simp [reverse]
+            rw [trd5]
+            rw [ih]
+            rw [trd1]
+            simp [concat]
+
 
 
 def map {A B : Type} : (A → B) → List A → List B :=
@@ -62,6 +133,8 @@ theorem map_concat {A B : Type} {f : A → B} {xs ys : List A} : map f (concat x
 
 theorem map_reverse {A B : Type} {f : A → B} {xs : List A} : map f (reverse xs) = reverse (map f xs) :=
   sorry
+
+-- Tukaj se zacnejo drevesa 
 
 inductive tree (A : Type) : Type where
   | empty : tree A
@@ -89,10 +162,20 @@ theorem max_comm {a b : Nat} : Nat.max a b = Nat.max b a :=
   sorry
 
 def mirror {A : Type} : tree A → tree A :=
-  sorry
+  fun t => 
+    match t with
+      | tree.empty => tree.empty
+      | tree.node x l r => tree.node x (mirror r) (mirror l)  
 
 theorem mirror_depth {A : Type} {t : tree A} : depth (mirror t) = depth t :=
-  sorry
+  by 
+    induction t with 
+    | empty => simp [mirror]
+    | node x l r ihl ihr => 
+      simp [mirror] 
+      simp [depth] 
+      rw [ihr, ihl]
+      rw [max_comm]
 
 theorem mirror_mirror {A : Type} {t : tree A} : mirror (mirror t) = t :=
   sorry
@@ -116,3 +199,7 @@ def size {A : Type} : tree A → Nat :=
 
 theorem size_mirror {A : Type} {t : tree A} : size (mirror t) = size t :=
   sorry
+
+
+
+
